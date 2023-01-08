261995bcf3a77c5b1b4edb3f8edcf50ecaf07108
Josiah VanderZee <josiah_vanderzee@mediacombb.net>
2023-01-08 16:01:56 -0600

    add market node

--- mods/core/market/init.lua
+++ mods/core/market/init.lua
@@ -8,3 +8,24 @@ commoditymarket.register_market("market", {
     allow_item = function(item) return true end,
     anonymous = true
 })
+
+minetest.register_node("market:market", {
+    description = "Market",
+    on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
+        assert(clicker ~= nil)
+        if not clicker:is_player() then
+            return
+        end
+        commoditymarket.show_market("market", clicker:get_player_name())
+    end
+})
+
+minetest.register_decoration({
+    deco_type = "simple",
+    place_on = {"default:dirt_with_grass", "default:sand"},
+    sidelen = 16,
+    fill_ratio = 0.001,
+    y_max = 200,
+    y_min = -50,
+    decoration = "market:market"
+})

f534370a947d44c314d3be9e8a0577589c4b64d9
Josiah VanderZee <josiah_vanderzee@mediacombb.net>
2023-01-08 14:15:44 -0600

    add initial market items

--- mods/core/market/init.lua
+++ mods/core/market/init.lua
@@ -1,8 +1,10 @@
 commoditymarket.register_market("market", {
-	description = "Market",
-	currency = {["default:dirt"] = 1},
-	inventory_limit = 1000,
-	sell_limit = 100,
-	allow_item = function(item) return true end,
-	anonymous = true
+    description = "Market",
+    currency = {["default:dirt"] = 1},
+    inventory_limit = 1000,
+    sell_limit = 100,
+    initial_items = {"default:apple", "farming:bread",
+                     "default:sword_wood", "default:sword_stone"},
+    allow_item = function(item) return true end,
+    anonymous = true
 })

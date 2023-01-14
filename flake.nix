f325c84d8db9954dccd0617deee1e42a9689fb6b
LizeLive <git@lize.live>
2023-01-14 11:23:32 -0800

    nix maybe?

--- flake.nix
+++ flake.nix
@@ -1,11 +1,56 @@
 {
   description = "A very basic flake";
-
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+  inputs.nixpkgs.url = "nixpkgs/nixos-22.11";
+  # inputs.c_mobs_redo = "https://notabug.org/tenplus1/mobs_redo.git";
+  # inputs.c_mobs_redo.flake = false;
+  # inputs.c_hudbars = "https://codeberg.org/Wuzzy/minetest_hudbars.git";
+  # inputs.c_hudbars.flake = false;
+  # inputs.c_player_monoids = "https://github.com/minetest-mods/player_monoids.git";
+  # inputs.c_player_monoids.flake = false;
+  # inputs.c_hbhunger = "https://codeberg.org/Wuzzy/minetest_hbhunger.git";
+  # inputs.c_hbhunger.flake = false;
+  # inputs.c_i3 = "https://github.com/minetest-mods/i3.git";
+  # inputs.c_i3.flake = false;
+  # inputs.c_hbsprint = "https://github.com/minetest-mods/hbsprint.git";
+  # inputs.c_hbsprint.flake = false;
+  # inputs.c_3d_armor = "https://github.com/minetest-mods/3d_armor.git";
+  # inputs.c_3d_armor.flake = false;
+  # inputs.c_newplayer = "https://cheapiesystems.com/git/newplayer";
+  # inputs.c_newplayer.flake = false;
+  # inputs.c_textures = "https://github.com/MysticTempest/REFI_Textures.git";
+  # inputs.c_textures.flake = false;
+  # inputs.c_splooshy_bombs = "https://codeberg.org/ekl/splooshy_bombs.git";
+  # inputs.c_splooshy_bombs.flake = false;
+  # inputs.c_splooshy_bombs_api = "https://codeberg.org/ekl/splooshy_bombs_api.git";
+  # inputs.c_splooshy_bombs_api.flake = false;
+  # inputs.c_playereffects = "https://repo.or.cz/minetest_playereffects.git";
+  # inputs.c_playereffects.flake = false;
+  # inputs.c_creatura = "https://github.com/ElCeejo/creatura.git";
+  # inputs.c_creatura.flake = false;
+  # inputs.c_hudbars = "https://codeberg.org/Wuzzy/minetest_hudbars.git";
+  # inputs.c_hudbars.flake = false;
+  outputs = { self, nixpkgs }: 
+  let 
+  newDrv = with nixpkgs.legacyPackages.x86_64-linux; stdenv.mkDerivation {
+    name = "minetestserver";
+    src = ./.;
+    buildInputs = [ minetestserver ];
+    phases = [ "buildPhase" ];
+    buildPhase = ''
+      mkdir -p $out/bin
+      cp ${minetestserver}/bin/minetestserver $out/bin/minetestserver
+      # substituteAllInPlace $out/bin/minetestserver \
+      #   --replace ${minetestserver}/share $out/share
+      sed -i "s|${minetestserver}/share|$out/share|g" $out/bin/minetestserver
+      cp -r ${minetestserver}/share/ $out/share/
+      chmod +w $out/share/minetest/games/
+      ls -l $out/share/minetest/games/
+      cp -r $src/ $out/share/minetest/games/ld52/
+    '';
+  };
+  in
+  {
+    packages.x86_64-linux.default = newDrv;
 
   };
 }
---
+++ mod.nix
@@ -0,0 +1,6 @@
+{pkgs} : pkgs.fetchFromGitHub {
+  owner = "MysticTempest";
+  repo = "REFI_Textures";
+  rev = "a24cdd32f5bc5b075c9e929416e7b617ddc63dcd";
+  hash = "sha256-C8MkXKIIi1R/ehxotuUYbL0R3BxSTIk7c5HZrnZH+iI=";
+}
